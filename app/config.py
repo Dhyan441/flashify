cc5867682d1275954cdbc0f2cb7392ace55a8b3c
Dhyan441 <dxxxx441@gmail.com>
2023-10-07 14:18:56 -0400

    Initial Flask Setup

---
+++ app/config.py
@@ -0,0 +1,31 @@
+import os
+import string
+import random
+
+
+letters = string.ascii_letters
+
+
+class BaseConfig(object):
+    """base config"""
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("secret_key", ''.join(
+        [random.choice(string.ascii_letters + string.digits) for n in range(16)]))
+
+
+class TestingConfig(BaseConfig):
+    """testing config"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = ''
+    DEBUG = True
+
+
+class DevelopmentConfig(BaseConfig):
+    """dev config"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = ''
+
+
+class ProductionConfig(BaseConfig):
+    """production config"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get("")
