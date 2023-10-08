ea83fb396ef5f4348aa1ba24d89fc43f0ba16dab
Dhyan441 <dxxxx441@gmail.com>
2023-10-07 22:04:47 -0400

    the signin and signup page + api scripts

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,28 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleCard = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
-    <div className="flex w-full h-screen">login
-        <div className = "w-full flex items-center justify-center lg:w-1/2">
-{/* form */}
-        <h1>Wlcome Back</h1>
-        <p>Welcome back! Please sign in.</p>
-        <div>
+    <div className="flex w-full h-screen">
+      <div className="w-full flex items-center justify-center lg:w-1/2">
+        <div className='bg-white px-10 py-20 rounded-3xl border-gray-200'>
+          <h1 className='text-5xl font-semibold'>Welcome {isLogin ? 'Back' : 'New User'}</h1>
+          <p className='font-medium text-lg text-gray-500 mt-4'>{isLogin ? 'Welcome back! Please sign in.' : 'Join us today! Create an account.'}</p>
+          <div className="mt-8">
+            <label className='text-lg font-medium'>Email</label>
+            <input className='w-full bottom-2 border border-gray-500 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your email'></input>
+          </div>
+          <div className="mt-8">
+            <label className='text-lg font-medium'>Password</label>
+            <input className='w-full bottom-2 rounded-xl p-4 mt-1 bg-transparent border border-gray-500' placeholder='Enter your password'></input>
+          </div>
+          <div className='mt-8 flex justify-between items-center'>
             <div>
-                <label>Email</label>
-                <input></input>
+              <input type='checkbox' id="remember" />
+              <label className="ml-2 font-medium text-base" htmlFor="remember">Remember for 30 days</label>
             </div>
+            {isLogin ? (
+              <button className='ml-4 font-medium text-base text-violet-500'>Forgot Password</button>
+            ) : null}
+          </div>
+          <div className='mt-8 flex flex-col gap-y-4'>
+            <button
+              className='active:scale-[.98] active:duration-75 hover:scale-[1.02] ease-in-out transition-all py-3 rounded-xl bg-violet-500 text-white text-lg font-bold'
+            >
+              {isLogin ? 'Sign in' : 'Sign up'}
+            </button>
+            <button
+              onClick={toggleCard}
+              className='active:scale-[.98] active:duration-75 hover:scale-[1.02] ease-in-out transition-all py-3 rounded-xl border-2 border-gray-200'
+            >
+              {isLogin ? 'Sign up' : 'Back to Login'}
+            </button>
+          </div>
         </div>
-        </div>
-        <div className="hidden relative w-1/2 h-full lg:flex items-center justify-center bg-gray-200">
-        <div className="w-60 h-60 rounded-full bg-gradient-to-tr from-violet-500 to-pink-500 animate-spin"> 
-        <div className="w-full h-1/2 absolute bottom-0 bg-white/10" >
-
-            </div>
-            </div>
-        </div>
+      </div>
+      <div className="hidden relative w-1/2 h-full lg:flex items-center justify-center bg-gray-200">
+        <div className="w-60 h-60 rounded-full bg-gradient-to-tr from-violet-500 to-pink-500 animate-bounce" />
+        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
