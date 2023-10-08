036049bdbc8df50ad3d1b770f78847d47503a5fe
Nihal Menon <=>
2023-10-07 22:18:46 -0400

    update environment variables

--- server/aiAPI/fetchFlash.py
+++ server/aiAPI/fetchFlash.py
@@ -1,8 +1,12 @@
 import openai
+import os
+from dotenv import find_dotenv, load_dotenv
 
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
 
 def flashCards (input) -> list[dict]:
-    api_key = "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" 
+    api_key = os.getenv("OpenAI_KEY", default=None)
 
     prompt = "Give me an array of 5 objects with fields prompt and answer where prompt has the question and answer has the answer use this input as the material:" + input
 
