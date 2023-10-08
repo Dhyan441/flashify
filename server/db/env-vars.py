989cb6363dc07a51163bc0ddf4dcc518de78cb6e
Nihal Menon <=>
2023-10-07 22:08:25 -0400

    deleted excess functions/files

--- server/db/env-vars.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import find_dotenv, load_dotenv
-
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
-
-CLOUD_SQL_USERNAME = os.getenv("CLOUD_SQL_USERNAME", default=None)
-print(CLOUD_SQL_USERNAME)
