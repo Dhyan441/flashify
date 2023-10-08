4a8597865fba51ea345a2e1e65b923d66f7760b1
official-larkin <ddlarkin@uwaterloo.ca>
2023-10-07 22:16:03 -0400

    dashboard and router changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
-import "./index.css";
-import Login from "./components/Login";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import AppLayout from "./components/AppLayout";
+import Decks from "./components/Decks";
 
 const App = () => {
   return (
-  <div>  <Login/> </div>
-
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/app/*" element={<AppLayout />}>
+            <Route path="decks" element={<Decks />} />
+          </Route>
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} /> */}
+          {/* <Route path="*" element={<ErrorPage />} /> */}
+        </Routes>
+      </Router>
+      {/* <Dashboard /> */}
+    </div>
   );
 };
 
 export default App;
-
